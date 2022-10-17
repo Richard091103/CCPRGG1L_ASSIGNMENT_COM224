7784664977f59126b6aba1eded4c0ccdfdfbaff0
Richard Jefferson Meneses <111546377+Richard091103@users.noreply.github.com>
2022-10-17 20:01:42 +0800

    Update assignment2.java

--- assignment2.java
+++ assignment2.java
@@ -9,25 +9,37 @@ public class App {
         // Act 1 - Print FIRST NAME 3 times using while loop
 
         int count = 0;
+        int i = 1;
         
         while (count != 3) {
-            System.out.println("RICHARD JEFFERSON");
+            System.out.println("(" + i + ".) RICHARD JEFFERSON");
             count++;
+            i++;
         }
 
+        System.out.println();
+
         // Act 2 - Print MIDDLE NAME 4 times using Do-While loop
-        
+
         int count2 = 0;
+        int y = 1;
         do {
-            System.out.println("VALERIANO");
+            System.out.println("(" + y + ".) VALERIANO");
             count2++;
-        } while (count2 <= 4);
+            y++;
+        } while (count2 < 4);
+
+        System.out.println();
 
         // Act 3 - Print SURNAME 5 times using for loop
         
+        int a = 1;
         for (count = 0; count < 5; count++) {
-            System.out.println("MENESES");
+            System.out.println("(" + a + ".) MENESES");
+            a++;
         }
+
+        System.out.println();
 
         // Act 4 - Create a char array that consists all of the characters of your FIRSTNAME and use a for loop to print each character of your FIRSTNAME. (example: ELIZER)
 
@@ -39,9 +51,12 @@ public class App {
 
         //or 
 
-        for (int x = 0; x < initial.length; x++) {
-            System.out.print(initial[x]);
-        }
+        //for (int x = 0; x < initial.length; x++) {
+        //    System.out.print(initial[x]);
+        //}
+
+        System.out.println();
+        System.out.println();
 
         // Act 5 - char array REVERSE First Name
         
@@ -51,7 +66,6 @@ public class App {
             System.out.print(ini[x]);
         }
         
-
         // Close Scanner
         scan.close();
     }
